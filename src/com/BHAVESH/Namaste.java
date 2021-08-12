93f0a39e1afda1aaaacd1407a722cc1a22c5e6b0
bhaveshgarg2000 <bhaveshgarg20052gmail.com>
2021-08-12 17:12:27 +0530

    day 1

---
+++ src/com/BHAVESH/Namaste.java
@@ -0,0 +1,19 @@
+package com.BHAVESH;
+
+public class Namaste {
+    public static void main(String[] args) {
+        // namaste in hindi
+        System.out.println("नमस्ते भारत");
+        // namaste in english
+        System.out.println("NAMASTE INDIA");
+        // namaste in spanish
+        System.out.println("Saludos");
+        // namaste in indonesian
+        System.out.println("Saludos");
+        // namaste in german
+        System.out.println("Grüße");
+        // namaste in japanese
+        System.out.println("ご挨拶");
+
+    }
+}
---
+++ src/com/BHAVESH/tut.java
@@ -0,0 +1,21 @@
+package com.BHAVESH;
+
+
+public class tut {
+    public static void main(String[] args) {
+
+        int aNumber = 3;
+        if (aNumber>=0){
+            System.out.println("First string");
+        }
+        else if (aNumber == 0) {
+            System.out.println("second string");
+
+        }
+else {
+            System.out.println("third string");
+        }
+
+            }
+    }
+
