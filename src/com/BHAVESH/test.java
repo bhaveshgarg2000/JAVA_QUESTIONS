4bae91e4172ff703bdc455901e2f7e991cf68d18
bhaveshgarg2000 <bhaveshgarg20052gmail.com>
2021-08-12 17:12:27 +0530

    tut

---
+++ src/com/BHAVESH/test.java
@@ -0,0 +1,18 @@
+package com.BHAVESH;
+
+public class test {
+    public static void main(String[] args) {
+        int aNumber = 3;
+        if (aNumber>=0){
+            System.out.println("First string");
+        }
+        else if (aNumber == 0) {
+            System.out.println("second string");
+
+        }
+        else {
+            System.out.println("third string");
+        }
+
+    }
+}
