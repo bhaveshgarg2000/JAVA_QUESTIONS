1df89221c59d60f5bfb740cc0fa608b3ef1df632
bhaveshgarg2000 <bhaveshgarg20052gmail.com>
2021-08-13 18:27:20 +0530

    DAY 2

--- src/com/BHAVESH/DAY2.java
+++ src/com/BHAVESH/DAY2.java
@@ -4,6 +4,7 @@ public class DAY2 {
     public static void main(String[] args) {
     int a = 50;
     int b = 20;
+    float c =100;
         if (a<b){
             System.out.println("A is greater than B");
         } else
