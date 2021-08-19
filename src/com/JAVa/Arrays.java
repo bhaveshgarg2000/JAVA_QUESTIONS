797c7f4d5c02a0242c3af53033e3773efb40c28f
bhaveshgarg2000 <bhaveshgarg20052gmail.com>
2021-08-19 17:37:55 +0530

    JAVA COURSE

--- src/com/JAVa/Arrays.java
+++ src/com/JAVa/Arrays.java
@@ -1,6 +1,8 @@
 package com.JAVa;
 
-public class Arrays {
+
+
+ class Arrays{
     public static void main(String[] args) {
         // array function
         int marks[] = new int[5];
@@ -15,5 +17,22 @@ public class Arrays {
         System.out.println(marks[2]);
         System.out.println(marks[3]);
         System.out.println(marks[4]);
+
+        //length of array
+        System.out.print("The length of array : ");
+        System.out.println(marks.length);
+    //array using known values
+        int[] mark = {100,200,300,400};
+        System.out.println("Array elements : " +mark[2]);
+
+        //2D array
+        int [][] finalMarks = {{90,200,300},{100,20,30}};
+        System.out.println("****2D Array Elements****");
+        System.out.print("Array Elements : ");
+        System.out.print(finalMarks[0][2]);
+
+
+
+
     }
 }
---
+++ src/com/JAVa/Casting.java
@@ -0,0 +1,20 @@
+package com.JAVa;
+
+public class Casting {
+    public static void main(String[] args) {
+     double price = 100.00;
+     double finalprice = price + 18;
+        System.out.println(finalprice);
+
+        int p = 100;
+        int fp =  p + (int)18.180;
+        System.out.println(fp);
+
+
+        //Constants
+
+        final float PI = 3.14F;
+        System.out.println(PI);
+
+    }
+}
