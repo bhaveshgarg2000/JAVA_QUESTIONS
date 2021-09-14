d9ade14cbbae24d03e08164058ca00a7692549b3
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2021-09-15 01:10:31 +0530

    SUPER KEYWORD

---
+++ src/com/practice/SuperKeyword.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+
+class Animal{
+
+    Animal(){
+        System.out.println("Animal is created");
+    }
+}
+    class God extends Animal{
+        God(){
+        System.out.println("God is created");
+
+        }
+    }
+
+
+public class SuperKeyword {
+    public static void main(String[] args) {
+       God d = new God();
+    }
+}
